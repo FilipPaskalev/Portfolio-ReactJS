9359e5f24f14193ab16282643158e2d787cf08b7
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2024-04-09 11:33:49 +0100

    refactor

--- src/pages/pageNotFound/PageNotFound.tsx
+++ src/pages/pageNotFound/PageNotFound.tsx
@@ -1,13 +1,7 @@
-import { Link } from "react-router-dom";
+import "./style.css";
 
 const PageNotFound = () => {
-  return (
-    <div>
-      <h1>Page Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
-      <Link to="/">Go back to home</Link>
-    </div>
-  );
+  return <section></section>;
 };
 
 export default PageNotFound;
