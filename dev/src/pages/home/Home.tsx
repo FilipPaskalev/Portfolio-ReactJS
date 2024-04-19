9b6b2015d13097569071382cebedda3de29410be
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2024-04-19 20:34:28 +0100

    add nav to home page

--- dev/src/pages/home/Home.tsx
+++ dev/src/pages/home/Home.tsx
@@ -1,11 +1,12 @@
+import { Container } from "react-bootstrap";
 import CustomNav from "../../components/customNav/CustomNav";
 
 const Home = () => {
   return (
-    <div>
+    <Container>
       <CustomNav />
       <h1>Home</h1>
-    </div>
+    </Container>
   );
 };
 
