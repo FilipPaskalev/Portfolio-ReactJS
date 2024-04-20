cc620f094489904cf9bdd37e2345ee7be34f48e1
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2024-04-20 23:22:16 +0100

    add json data

--- dev/src/components/navigation/Navigation.tsx
+++ dev/src/components/navigation/Navigation.tsx
@@ -1,8 +1,13 @@
+// Global styles
 import '../../../styles.css';
-import '../../data/contact.json';
+// Component styles
+import './navigation.css';
+// Data
+import userInfoData from '../../data/userInfo.json';
+// Assets
 import avatar from '../../assets/avatar.png';
 
-type ContactTypes = {
+type UserInfoTypes = {
   firstName: string;
   lastName: string;
   nickName: string;
@@ -17,25 +22,13 @@ type ContactTypes = {
 };
 
 const Navigation = () => {
-  const contact: ContactTypes = {
-    firstName: 'Filip',
-    lastName: 'Paskalev',
-    nickName: 'Phil',
-    email: 'paskalevfilip@gmail.com',
-    phoneNumber: '+44 7882 146 424',
-    city: 'Birmingham',
-    country: 'UK',
-    linkedIn: 'https://www.linkedin.com/',
-    facebook: 'https://www.facebook.com/',
-    twitter: 'https://twitter.com/',
-    github: 'https://github.com/',
-  };
+  const { firstName, lastName } = userInfoData as UserInfoTypes;
 
   return (
     <nav className='navbar navbar-expand-lg navbar-dark bg-primary fixed-top' id='sideNav'>
       <a className='navbar-brand js-scroll-trigger' href='#page-top'>
         <span className='d-block d-lg-none'>
-          {contact.firstName} {contact.lastName}
+          {firstName} {lastName}
         </span>
         <span className='d-none d-lg-block'>
           <img className='img-fluid img-profile rounded-circle mx-auto mb-2' src={avatar} alt='profile avatar' />
