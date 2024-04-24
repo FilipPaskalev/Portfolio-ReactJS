1401e764de129f59ed4842a1343aca2a308437e4
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2024-04-25 00:31:51 +0100

    refactor

--- dev/src/components/about/footer/Footer.tsx
+++ dev/src/components/about/footer/Footer.tsx
@@ -16,7 +16,14 @@ const Footer = (props: IFooterProps) => {
   return (
     <footer className='social-icons'>
       {props.icons.map((icon, index) => (
-        <a className='social-icon' href={icon.link} target={icon.target} key={index}>
+        <a
+          key={index}
+          href={icon.link}
+          data-bs-toggle='tooltip'
+          data-bs-placement='top'
+          title={icon.text}
+          className='social-icon'
+          target={icon.target}>
           <i className={icon.icon}></i>
         </a>
       ))}
