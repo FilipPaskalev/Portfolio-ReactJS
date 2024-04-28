48b0d7e4394b5a263075a10fb61867311122c59d
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2024-04-28 12:01:44 +0100

    delete

--- dev/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom/client';
-import './app.css';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <div>homepage</div>
-  </React.StrictMode>,
-);
