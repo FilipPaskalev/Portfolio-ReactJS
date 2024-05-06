f7a89025c6119087166b90e4209973f193929193
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2024-05-06 13:02:14 +0100

    add structure, types, data

--- dev/src/components/navigation/sideNav/SideNav.tsx
+++ dev/src/components/navigation/sideNav/SideNav.tsx
@@ -1,22 +1,57 @@
 import "./side-nav-styles.scss";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
-import { Image, Nav, Row } from "react-bootstrap";
+import { Col, Image, Nav, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import EPagePaths from "../../../enum/EPagePaths";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const SideNav = () => {
   return (
     <Navbar>
       <Container>
-        <Row>
-          <Image src="https://via.placeholder.com/150" roundedCircle />
-          <Navbar.Brand className="mx-2">Something here</Navbar.Brand>
+        <Row className="d-flex justify-content-center align-items-center">
+          <Navbar.Brand className="mx-2 text-center">
+            Something here
+          </Navbar.Brand>
+          <Col
+            xl={8}
+            xxl={8}
+            className="d-flex justify-content-center align-items-center mt-5"
+          >
+            <Image
+              src="https://via.placeholder.com/150"
+              roundedCircle
+              className="w-75 mx-auto "
+            />
+          </Col>
           <Nav>
-            <Container>
-              <Row>
-                <Link to={EPagePaths.HOME}>Home</Link>
-                <Link to={EPagePaths.ABOUT_THIS_PROJECT}>About</Link>
+            <Container className="mt-5">
+              <Row className="text-center mb-5">
+                <Col xl={12} className="mb-3">
+                  <FontAwesomeIcon icon="house" className="mx-2" />
+                  <Link to={EPagePaths.HOME}>Home</Link>
+                </Col>
+                <Col xl={12} className="mb-3">
+                  <FontAwesomeIcon icon="info-circle" className="mx-2" />
+                  <Link to={EPagePaths.ABOUT_THIS_PROJECT}>About</Link>
+                </Col>
+                <Col xl={12} className="mb-3">
+                  <FontAwesomeIcon icon="info-circle" className="mx-2" />
+                  <Link to={EPagePaths.ABOUT_THIS_PROJECT}>About</Link>
+                </Col>
+                <Col xl={12} className="mb-3">
+                  <FontAwesomeIcon icon="info-circle" className="mx-2" />
+                  <Link to={EPagePaths.ABOUT_THIS_PROJECT}>About</Link>
+                </Col>
+                <Col xl={12} className="mb-3">
+                  <FontAwesomeIcon icon="info-circle" className="mx-2" />
+                  <Link to={EPagePaths.ABOUT_THIS_PROJECT}>About</Link>
+                </Col>
+                <Col xl={12} className="mb-3">
+                  <FontAwesomeIcon icon="info-circle" className="mx-2" />
+                  <Link to={EPagePaths.ABOUT_THIS_PROJECT}>About</Link>
+                </Col>
               </Row>
             </Container>
           </Nav>
--- dev/src/pages/home/HomePage.tsx
+++ dev/src/pages/home/HomePage.tsx
@@ -1,29 +1,147 @@
+// Styles
 import "./home-page.scss";
+// Custom components
 import Navigation from "../../components/navigation/Navigation";
+// React Bootstrap Icons
+import * as Icon from "react-bootstrap-icons";
+// React Bootstrap components
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+
+type TAddress = {
+  street: string;
+  city: string;
+  state: string;
+  zip: string;
+};
+
+type TPhoneNumber = {
+  plain: string;
+  masked: string;
+};
 
 const HomePage = () => {
   const navMessage: string[] = [
     "Hi, there and Welcome",
     "This is my portfolio page!",
   ];
+  const firstName: string = "Clarence";
+  const lastName: string = "Taylor";
+  const address: TAddress = {
+    street: "3542 Berry Street",
+    city: "Cheyenne Wells",
+    state: "CO",
+    zip: "80810",
+  };
+  const phoneNumber: TPhoneNumber = {
+    plain: "+5555555555",
+    masked: "(555) 555-5555",
+  };
+  const email: string = "myEmailAddress@example.com";
+  const socialLinks = {
+    linkedIn: "https://www.linkedin.com/in/paskalevfilip",
+    gitHub: "https://github.com/",
+    discord: "https://discord.com/",
+    facebook: "https://www.facebook.com/",
+  };
 
   return (
     <Container fluid>
       <Row>
-        <Col xl={2} xxl={2}>
+        <Col xl={4} xxl={4} className="bg-secondary">
           <Navigation message={navMessage} />
         </Col>
-        <Col xl={10} xxl={10}>
-          <p>Home page</p>
-          <p>
-            Ullamco consequat quis tempor exercitation labore est sit ullamco
-            voluptate occaecat. Sint eu fugiat voluptate sunt do laborum mollit
-            reprehenderit. Veniam in proident consequat laboris consectetur
-            commodo.
-          </p>
+        <Col xl={8} xxl={8}>
+          <h1 className="mt-4 mb-3">
+            <b>
+              {firstName}
+              <span className="text-primary">{lastName}</span>
+            </b>
+          </h1>
+          <address className="mb-5">
+            <Col className="mb-1">
+              <Icon.GeoAltFill size={20} color="primary" />
+              {"  "}
+              {address.street}
+              {" · "}
+              {address.city}
+              {" · "}
+              {address.state} {address.zip}
+            </Col>
+            <Col>
+              <Icon.Phone size={20} color="primary" />
+              {"  "}
+              <a href={`tel:${phoneNumber.plain}`}>{phoneNumber.masked}</a>
+            </Col>
+            <Col>
+              <Icon.Envelope size={20} color="primary" />
+              {"  "}
+              <a href={`mailto:${email}`}>{email}</a>
+            </Col>
+          </address>
+          <section>
+            <header className="mb-4 display-1">
+              <b>Hi, there !</b>
+            </header>
+            <p className="lead mb-4">
+              Ea consequat magna aliquip veniam cillum duis tempor ex Lorem
+              cupidatat ullamco irure. Irure laboris elit elit minim mollit qui
+              quis voluptate fugiat excepteur minim est irure. Incididunt dolor
+              duis ipsum id. Amet occaecat amet dolor do. Sit do do amet aliquip
+              duis ipsum id. Amet occaecat amet dolor do. Sit do do amet aliquip
+              duis ipsum id. Amet occaecat amet dolor do. Sit do do amet aliquip
+            </p>
+            <footer className="text-center mt-4 fixed-bottom">
+              <Container className="mx-auto">
+                <Row className="justify-content-center">
+                  <Col>
+                    <a
+                      href={socialLinks.linkedIn}
+                      target="_blank"
+                      className="mx-2"
+                    >
+                      <Icon.Linkedin
+                        size={38}
+                        title="LinkedIn"
+                        color="primary"
+                      />
+                    </a>
+                    <a
+                      href={socialLinks.gitHub}
+                      target="_blank"
+                      className="mx-2"
+                    >
+                      <Icon.Github size={38} title="GitHub" color="primary" />
+                    </a>
+                    <a
+                      href={socialLinks.discord}
+                      target="_blank"
+                      className="mx-2"
+                    >
+                      <Icon.Discord size={38} title="Discord" color="primary" />
+                    </a>
+                    <a
+                      href={socialLinks.facebook}
+                      target="_blank"
+                      className="mx-2"
+                    >
+                      <Icon.Facebook
+                        size={38}
+                        title="Facebook"
+                        color="primary"
+                      />
+                    </a>
+                  </Col>
+                </Row>
+                <Row className="justify-content-center mt-4">
+                  <p className="text-muted">
+                    &copy; {new Date().getFullYear()} {firstName} {lastName}
+                  </p>
+                </Row>
+              </Container>
+            </footer>
+          </section>
         </Col>
       </Row>
     </Container>
